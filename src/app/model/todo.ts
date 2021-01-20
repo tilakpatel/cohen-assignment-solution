e8813a81935dc5ded4600370dbe1c3051833b04b
tilakpatel <77743017+tilakpatel@users.noreply.github.com>
2021-01-21 01:59:47 +0530

    done

--- src/app/model/todo.ts
+++
@@ -1,9 +0,0 @@
-export class Todo {
-    constructor(
-        public id: number,
-        public description: string,
-        public dueDate: string,
-        public priority: number,
-        public done?: boolean,
-    ) { }
-}
